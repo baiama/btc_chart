2eabd858d02cecb24a399cc7af9691e68fef915c
baiama <primkulovbaiaman@gmail.com>
2021-11-06 15:31:17 +0600

    dsa

--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -1,14 +1,26 @@
+import 'package:btc_chart/logic/cubits/home_page_cubit.dart';
+import 'package:btc_chart/logic/cubits/home_page_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text('BTC/AUD'),
+    return BlocProvider(
+      create: (context) => HomePageCubit()..loadData(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text('BTC/AUD'),
+        ),
+        body: BlocConsumer<HomePageCubit, HomePageState>(
+          listener: (BuildContext context, state) {},
+          builder: (BuildContext context, state) {
+            return Container();
+          },
+        ),
       ),
     );
   }
