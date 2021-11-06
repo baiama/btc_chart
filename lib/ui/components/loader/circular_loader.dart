aeb3e749a30f83c7d6dde303a3e616bdeff7d232
baiama <primkulovbaiaman@gmail.com>
2021-11-06 17:29:02 +0600

    ad

---
+++ lib/ui/components/loader/circular_loader.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CircularLoader extends StatelessWidget {
+  const CircularLoader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      padding: const EdgeInsets.only(top: 100),
+      child: const CircularProgressIndicator(),
+    );
+  }
+}
--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -1,7 +1,9 @@
 import 'package:btc_chart/logic/cubits/home_page_cubit.dart';
 import 'package:btc_chart/logic/cubits/home_page_state.dart';
+import 'package:btc_chart/logic/models/btc_model.dart';
 import 'package:btc_chart/presentation/app_colors.dart';
 import 'package:btc_chart/presentation/app_text_styles.dart';
+import 'package:btc_chart/ui/components/loader/circular_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -30,10 +32,26 @@ class HomePage extends StatelessWidget {
             }
           },
           builder: (BuildContext context, state) {
+            if (state is HomePageStateLoading) {
+              return const CircularLoader();
+            }
+            if (state is HomePageStateLoaded) {
+              return _HomePageLoaded(btcModel: state.btcData);
+            }
             return Container();
           },
         ),
       ),
     );
+  }
+}
+
+class _HomePageLoaded extends StatelessWidget {
+  final BTCModel btcModel;
+  const _HomePageLoaded({Key? key, required this.btcModel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
