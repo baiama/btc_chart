36ec2c2f0e96b946373c1b8b823a556c961095a7
baiama <primkulovbaiaman@gmail.com>
2021-11-06 17:12:01 +0600

    dsa

--- lib/logic/api/btc_api.dart
+++ lib/logic/api/btc_api.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 
 import 'package:btc_chart/logic/api/api.dart';
 import 'package:btc_chart/logic/models/btc_model.dart';
+import 'package:btc_chart/logic/models/http/response_helper.dart';
 import 'package:dio/dio.dart';
 
 const String _baseUrl = "https://app.independentreserve.com/apim/";
 
 class BTCApi extends ApiMap {
+  //Set base url and header for requests
   @override
   FutureOr<BaseOptions> getOptions() {
     var options = BaseOptions(
@@ -16,15 +18,15 @@ class BTCApi extends ApiMap {
     return options;
   }
 
-  Future<dynamic> getBTCData() async {
+  Future<ResponseData<BTCModel>> getBTCData() async {
     try {
       var client = await getClient();
       Response res = await client.get(
           'market/chart?model.range=1d&model.primary=Xbt&model.secondary=Aud');
       client.close();
-      return BTCModel.fromJson(res.data);
+      return ResponseData(data: BTCModel.fromJson(res.data), error: null);
     } on DioError catch (e) {
-      return e.toString();
+      return ResponseData(data: null, error: e.toString());
     }
   }
 }
--- lib/logic/models/http_reponce/reponse_helper.dart
+++
@@ -1,6 +0,0 @@
-class ResponseData<T> {
-  final T? data;
-  String? error;
-
-  ResponseData({required this.data, required this.error});
-}
