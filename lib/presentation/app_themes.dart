4e815327bb24ffd5f736b23736ae98e3e3e00ad8
baiama <primkulovbaiaman@gmail.com>
2021-11-06 17:23:39 +0600

    ads

--- lib/presentation/app_themes.dart
+++ lib/presentation/app_themes.dart
@@ -5,7 +5,8 @@ import 'app_text_styles.dart';
 
 class AppThemes {
   static const appBar = AppBarTheme(
-      titleTextStyle: AppTextStyles.appBarTitle,
-      backgroundColor: AppColors.white,
-      elevation: 0.0);
+    titleTextStyle: AppTextStyles.appBarTitle,
+    backgroundColor: AppColors.white,
+    elevation: 0.0,
+  );
 }
